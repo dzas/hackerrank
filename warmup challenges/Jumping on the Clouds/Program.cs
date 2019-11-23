e912039a974e5b7f9ea50a7b847bbba5e730732b
asdz <asdz@dzas.ru>
2019-11-23 23:33:18 +0300

    jumps solved

---
+++ warmup challenges/Jumping on the Clouds/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+
+namespace Jumping_on_the_Clouds
+{
+    class Program
+    {
+        enum CloudType
+        {
+            Cumulus = 0,
+            Thunderhead = 1
+        }
+
+        static int JumpingOnClouds(int[] clouds)
+        {
+            int cloudIndex = 0;
+            int jumps = 0;
+            int shortJumpLength = 1;
+            int farJumpLength = 2;
+
+            while (cloudIndex < clouds.Length - 1)
+            {
+                int jumpLength;
+
+                if ((cloudIndex + farJumpLength <= clouds.Length - 1) &&
+                    (CloudType)clouds[cloudIndex + farJumpLength] == CloudType.Cumulus)
+                {
+                    jumpLength = farJumpLength;
+                }
+                else
+                {
+                    jumpLength = shortJumpLength;
+                }
+
+                cloudIndex += jumpLength;
+                jumps++;
+            }
+
+            return jumps;
+        }
+
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int cloudsCount = Convert.ToInt32(Console.ReadLine());
+
+            int[] clouds = Array.ConvertAll(Console.ReadLine().Split(' '), cTemp => Convert.ToInt32(cTemp));
+
+            int result = JumpingOnClouds(clouds);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
