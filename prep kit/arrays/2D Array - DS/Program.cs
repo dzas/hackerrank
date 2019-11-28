b692d4e28c46337156dc928a7f3ae181fd6a9089
asdz <asdz@dzas.ru>
2019-11-28 22:06:48 +0300

    wip

---
+++ prep kit/arrays/2D Array - DS/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+namespace _2D_Array___DS
+{
+    class Program
+    {
+        int HourglassSum(int[,] arr)
+        {
+            int[,] subArray = new int[3, 3];
+            var w = arr.GetLength(0) / subArray.GetLength(0);
+            var h = arr.GetLength(1) / subArray.GetLength(1);
+            int[][,] glasses = new int[w * h][,];
+
+            for (int i = 0; i < w; i++)
+            {
+                for (int j = 0; i < h; i++)
+                {
+                    subArray
+                }
+            }
+
+        }
+
+
+        static void Main(string[] args)
+        {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int[,] arr = new int[6, 6];
+
+            for (int i = 0; i < arr.GetLength(0); i++)
+            {
+                var line = Console.ReadLine().Split(' ');
+
+                for (int j = 0; j < line.Length; j++)
+                {
+                    arr[i, j] = Convert.ToInt32(line[j]);
+                }
+            }
+
+            int result = HourglassSum(arr);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
+        }
+    }
+}
